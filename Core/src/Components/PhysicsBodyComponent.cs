a33bba80f1284910a88d410513d0fd7f185584b5
Bruno Henrique 'FluffyBucketSnake' Glowaski Morais <fluffybucketsnake@outlook.com>
2024-03-26 01:36:50 -0400

    feat: sync physic body and entity position

--- Core/src/Components/PhysicsBodyComponent.cs
+++ Core/src/Components/PhysicsBodyComponent.cs
@@ -3,7 +3,7 @@ using Microsoft.Xna.Framework;
 
 namespace MegamanX.Components
 {
-    public class PhysicsBodyComponent : IComponent
+    public class PhysicBodyComponent : IComponent
     {
         public PhysicBody Body { get; }
 
@@ -12,13 +12,19 @@ namespace MegamanX.Components
         public Vector2 Position { get => Body.Position; set => Body.Position = value; }
         public Vector2 Velocity { get => Body.Velocity; set => Body.Velocity = value; }
 
-        public PhysicsBodyComponent(Entity entity, PhysicWorld physicWorld, Rectangle bounds)
+        private readonly TransformComponent transformComponent;
+
+        public PhysicBodyComponent(Entity entity, PhysicWorld physicWorld, Rectangle bounds)
         {
+            transformComponent = entity.GetComponent<TransformComponent>();
+
             PhysicWorld = physicWorld;
 
             Body = new(bounds)
             {
-                UserData = entity
+                UserData = entity,
+                GravityScale = 1,
+                Position = transformComponent.Position
             };
             PhysicWorld.Bodies.Add(Body);
         }
@@ -28,6 +34,11 @@ namespace MegamanX.Components
             PhysicSensor sensor = new(bounds);
             Body.Sensors.Add(sensor);
             return sensor;
+        }
+
+        void IComponent.Update(GameTime gameTime)
+        {
+            transformComponent.Position = Body.Position;
         }
     }
 }
